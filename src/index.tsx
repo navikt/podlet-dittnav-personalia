f6e1b0229e4110ab4890603babccca2640940673
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-01-20 12:39:04 +0100

    Slår av refetching on error

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as Sentry from "@sentry/browser";
+import { SWRConfig } from "swr";
 import "./index.css";
 import App from "./App";
 
@@ -13,7 +14,9 @@ if (process.env.NODE_ENV === "development") {
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SWRConfig value={{ shouldRetryOnError: false }}>
+      <App />
+    </SWRConfig>
   </React.StrictMode>,
   document.getElementById("podlet-dittnav-personalia")
 );
