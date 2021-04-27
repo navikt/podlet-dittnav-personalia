b7c14731e5f51a4d2c9a5c39b713dd15803e7ca5
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-04-27 14:28:44 +0200

    La til disclaimer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import Personalia from "./components/Personalia";
+import AlertStripe from "nav-frontend-alertstriper";
+import { Undertittel } from "nav-frontend-typografi";
+import Lenke from "nav-frontend-lenker";
+import "./App.less";
 
 const App = () => (
   <div className="podlet-dittnav-personalia">
+    <div className="disclaimer">
+      <AlertStripe type="advarsel">
+        <Undertittel>Denne siden er under utvikling</Undertittel>
+        Du har kommet til en side som er under utvikling og bare skal prøves ut på utvalgte kontorer. Trykk
+        <Lenke href="https://www.nav.no/person/dittnav"> her</Lenke> for å gå til DittNAV.
+      </AlertStripe>
+    </div>
     <Personalia />
   </div>
 );
